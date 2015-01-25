415eca8e3b72db276bc4c023a829f8cb7a8ffbbd
Spencer Yen <spencersyen@gmail.com>
2015-01-25 00:21:39 -0800

    Initial Commit

---
+++ Common Roots for Counselors/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Common Roots for Counselors
+//
+//  Created by Spencer Yen on 1/25/15.
+//  Copyright (c) 2015 Parameter Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Common Roots for Counselors/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Common Roots for Counselors
+//
+//  Created by Spencer Yen on 1/25/15.
+//  Copyright (c) 2015 Parameter Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
